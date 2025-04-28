c654a046a67c90f53026b29ebe6246925362e7b2
youThink2 <daniel.mallia.g53483@mcast.edu.mt>
2025-04-28 15:04:06 +0200

    Layout update

---
+++ Website/includes/sidebar.php
@@ -0,0 +1,9 @@
+<div class="sidebar">
+    <div class="circle"></div>
+    <button>Admin Panel</button>
+    <button>My Library</button>
+    <button>Playlists</button>
+    <button>Playlists</button>
+    <button>Playlists</button>
+    <input type="text" placeholder="Search">
+</div>
---
+++ Website/index.php
@@ -0,0 +1,38 @@
+<!-- header included -->
+<?php
+    include 'includes/sidebar.php';
+?>
+
+<?php
+    include 'includes/header.php';
+?>
+
+
+<div class="content">
+    <div class="section">
+        <h2>Artists</h2>
+        <div class="artists">
+            <div class="artist"></div>
+            <div class="artist"></div>
+            <div class="artist"></div>
+            <div class="artist"></div>
+            <div class="artist"></div>
+        </div>
+    </div>
+    <div class="section">
+        <h2>Albums</h2>
+        <div class="albums">
+            <div class="album"></div>
+            <div class="album"></div>
+            <div class="album"></div>
+            <div class="album"></div>
+            <div class="album"></div>
+        </div>
+    </div>
+</div>
+
+
+
+<?php
+    include 'includes/footer.php';
+?>
